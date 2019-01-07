b2c93765227db8cd0e1bfac3e67bab260b852964
zhanghehe <709947764@qq.com>
2019-01-07 10:49:24 +0800

    初始化

--- app/src/main/java/com/zhh/base/mvvm/view/activity/MainActivity.java
+++ app/src/main/java/com/zhh/base/mvvm/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zhh.base;
+package com.zhh.base.mvvm.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zhh.base.R;
 
 public class MainActivity extends AppCompatActivity {
 
